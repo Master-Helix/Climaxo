0812555c542abcf0f4aa87ca5942b91b107b4d7c
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2021-11-10 23:57:13 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Temp from './Temp';
+
+ReactDOM.render(
+  <>
+    <Temp/>
+  </>,
+  document.getElementById('root')
+);
+
